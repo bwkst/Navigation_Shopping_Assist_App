0eee819f68989450545d3b4514e531617535b697
bwkst <s.t.kristine.or@gmail.com>
2021-08-03 01:31:04 +0800

    前端——微调

--- app/src/main/java/com/example/demo/MainActivity.java
+++ app/src/main/java/com/example/demo/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_confirm_market);
+        setContentView(R.layout.activity_main);
     }
 }
