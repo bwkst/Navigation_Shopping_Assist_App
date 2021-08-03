33400e898380331e1bbe9705a40110b6d9c10daa
bwkst <s.t.kristine.or@gmail.com>
2021-08-03 18:35:18 +0800

    前端——微调+调节与帮助
    主页面完成（未进行onClick对接）

--- app/src/main/java/com/example/demo/MainActivity.java
+++ app/src/main/java/com/example/demo/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_supermarket);
+        setContentView(R.layout.activity_setting_and_help);
     }
 }
