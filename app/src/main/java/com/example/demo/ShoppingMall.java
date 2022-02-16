5274cfa9c2962e15cca815374cf064d1e6ca4fd1
Aranwind <zong002230@gmail.com>
2022-02-16 09:51:44 +0800

    语音输入完成

--- app/src/main/java/com/example/demo/ShoppingMall.java
+++ app/src/main/java/com/example/demo/ShoppingMall.java
@@ -3,10 +3,36 @@ package com.example.demo;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.app.Activity;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import com.iflytek.cloud.RecognizerResult;
+import com.iflytek.cloud.SpeechConstant;
+import com.iflytek.cloud.SpeechError;
+import com.iflytek.cloud.SpeechRecognizer;
+import com.iflytek.cloud.SpeechUtility;
+import com.iflytek.cloud.ui.RecognizerDialog;
+import com.iflytek.cloud.ui.RecognizerDialogListener;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ShoppingMall extends AppCompatActivity {
+
+    //存放听写分析结果文本
+    private HashMap<String, String> hashMapTexts = new LinkedHashMap<>();
+    SpeechRecognizer hearer;  //听写对象
+    RecognizerDialog dialog;  //讯飞提示框
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +50,81 @@ public class ShoppingMall extends AppCompatActivity {
 
         //ShoppingMall -> 打开语音输入键盘
         Button MLGTVoiceKeyboard = findViewById(R.id.b_mall_voice_input); //定位Button: b_mall_voice_input
-        //功能未做-不会做
+        TextView MLGVoiceResult = findViewById(R.id.mall_destination_box);//定位TextView：mall_destination_box
+        /***语音听写功能实现***/
+        // 语音配置对象初始化
+        SpeechUtility.createUtility(ShoppingMall.this, SpeechConstant.APPID + "=f66cb08c");
+        MLGTVoiceKeyboard.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 1.创建SpeechRecognizer对象，第2个参数：本地听写时传InitListener
+                hearer = SpeechRecognizer.createRecognizer( ShoppingMall.this, null);
+                // 交互动画
+                dialog = new RecognizerDialog(ShoppingMall.this, null);
+                // 2.设置听写参数，详见《科大讯飞MSC API手册(Android)》SpeechConstant类
+                hearer.setParameter(SpeechConstant.DOMAIN, "iat"); // domain:域名
+                hearer.setParameter(SpeechConstant.LANGUAGE, "zh_cn");
+                hearer.setParameter(SpeechConstant.ACCENT, "mandarin"); // mandarin:普通话
+                hearer.setParameter(SpeechConstant.ASR_PTT, "0");//无标点
+
+                //3.开始听写
+                dialog.setListener(new RecognizerDialogListener() {  //设置对话框
+
+                    @Override
+                    public void onResult(RecognizerResult results, boolean isLast) {
+                        // TODO 自动生成的方法存根
+                        Log.d("Result", results.getResultString());
+                        //(1) 解析 json 数据<< 一个一个分析文本 >>
+                        StringBuffer strBuffer = new StringBuffer();
+                        try {
+                            JSONTokener tokener = new JSONTokener(results.getResultString());
+                            Log.i("TAG", "Test"+results.getResultString());
+                            Log.i("TAG", "Test"+results.toString());
+                            JSONObject joResult = new JSONObject(tokener);
+
+                            JSONArray words = joResult.getJSONArray("ws");
+                            for (int i = 0; i < words.length(); i++) {
+                                // 转写结果词，默认使用第一个结果
+                                JSONArray items = words.getJSONObject(i).getJSONArray("cw");
+                                JSONObject obj = items.getJSONObject(0);
+                                strBuffer.append(obj.getString("w"));
+
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+//            		String text = strBuffer.toString();
+                        // (2)读取json结果中的sn字段
+                        String sn = null;
+
+                        try {
+                            JSONObject resultJson = new JSONObject(results.getResultString());
+                            sn = resultJson.optString("sn");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        //(3) 解析语音文本<< 将文本叠加成语音分析结果  >>
+                        hashMapTexts.put(sn, strBuffer.toString());
+                        StringBuffer resultBuffer = new StringBuffer();  //最后结果
+                        for (String key : hashMapTexts.keySet()) {
+                            resultBuffer.append(hashMapTexts.get(key));
+                        }
+
+                        MLGVoiceResult.setText(resultBuffer.toString());
+
+                    }
+
+                    @Override
+                    public void onError(SpeechError error) {
+                        // TODO 自动生成的方法存根
+                        error.getPlainDescription(true);
+                    }
+                });
+
+                dialog.show();  //显示对话框
+            }
+        });
 
         //ShoppingMall -> Go To Page: MainActivity
         Button MLGTMainPage = findViewById(R.id.b_mall_back); //定位Button: b_mall_back
--- app/src/main/java/com/example/demo/Supermarket.java
+++ app/src/main/java/com/example/demo/Supermarket.java
@@ -5,13 +5,36 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ImageView;
-
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import com.iflytek.cloud.RecognizerResult;
+import com.iflytek.cloud.SpeechConstant;
+import com.iflytek.cloud.SpeechError;
+import com.iflytek.cloud.SpeechRecognizer;
+import com.iflytek.cloud.SpeechUtility;
+import com.iflytek.cloud.ui.RecognizerDialog;
+import com.iflytek.cloud.ui.RecognizerDialogListener;
+
 public class Supermarket extends AppCompatActivity {
+
+    //存放听写分析结果文本
+    private HashMap<String, String> hashMapTexts = new LinkedHashMap<>();
+    SpeechRecognizer hearer;  //听写对象
+    RecognizerDialog dialog;  //讯飞提示框
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +51,81 @@ public class Supermarket extends AppCompatActivity {
 
         //Supermarket -> 打开语音输入键盘
         Button MTGTVoiceKeyboard = findViewById(R.id.b_market_voice_input); //定位Button: b_market_voice_input
-        //功能未做-不会做
+        TextView MTGVoiceResult = findViewById(R.id.market_destination_box);//定位TextView：market_destination_box
+        /***语音听写功能实现***/
+        // 语音配置对象初始化
+        SpeechUtility.createUtility(Supermarket.this, SpeechConstant.APPID + "=f66cb08c");
+        MTGTVoiceKeyboard.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 1.创建SpeechRecognizer对象，第2个参数：本地听写时传InitListener
+                hearer = SpeechRecognizer.createRecognizer( Supermarket.this, null);
+                // 交互动画
+                dialog = new RecognizerDialog(Supermarket.this, null);
+                // 2.设置听写参数，详见《科大讯飞MSC API手册(Android)》SpeechConstant类
+                hearer.setParameter(SpeechConstant.DOMAIN, "iat"); // domain:域名
+                hearer.setParameter(SpeechConstant.LANGUAGE, "zh_cn");
+                hearer.setParameter(SpeechConstant.ACCENT, "mandarin"); // mandarin:普通话
+                hearer.setParameter(SpeechConstant.ASR_PTT, "0");//无标点
+
+                //3.开始听写
+                dialog.setListener(new RecognizerDialogListener() {  //设置对话框
+
+                    @Override
+                    public void onResult(RecognizerResult results, boolean isLast) {
+                        // TODO 自动生成的方法存根
+                        Log.d("Result", results.getResultString());
+                        //(1) 解析 json 数据<< 一个一个分析文本 >>
+                        StringBuffer strBuffer = new StringBuffer();
+                        try {
+                            JSONTokener tokener = new JSONTokener(results.getResultString());
+                            Log.i("TAG", "Test"+results.getResultString());
+                            Log.i("TAG", "Test"+results.toString());
+                            JSONObject joResult = new JSONObject(tokener);
+
+                            JSONArray words = joResult.getJSONArray("ws");
+                            for (int i = 0; i < words.length(); i++) {
+                                // 转写结果词，默认使用第一个结果
+                                JSONArray items = words.getJSONObject(i).getJSONArray("cw");
+                                JSONObject obj = items.getJSONObject(0);
+                                strBuffer.append(obj.getString("w"));
+
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+//            		String text = strBuffer.toString();
+                        // (2)读取json结果中的sn字段
+                        String sn = null;
+
+                        try {
+                            JSONObject resultJson = new JSONObject(results.getResultString());
+                            sn = resultJson.optString("sn");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        //(3) 解析语音文本<< 将文本叠加成语音分析结果  >>
+                        hashMapTexts.put(sn, strBuffer.toString());
+                        StringBuffer resultBuffer = new StringBuffer();  //最后结果
+                        for (String key : hashMapTexts.keySet()) {
+                            resultBuffer.append(hashMapTexts.get(key));
+                        }
+
+                        MTGVoiceResult.setText(resultBuffer.toString());
+
+                    }
+
+                    @Override
+                    public void onError(SpeechError error) {
+                        // TODO 自动生成的方法存根
+                        error.getPlainDescription(true);
+                    }
+                });
+
+                dialog.show();  //显示对话框
+            }
+        });
 
         //Supermarket -> Go To Page: MainActivity
         Button MTGTMainPage = findViewById(R.id.b_market_back); //定位Button: b_market_back
