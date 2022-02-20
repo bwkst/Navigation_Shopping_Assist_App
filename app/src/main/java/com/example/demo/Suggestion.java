935455f240f1dad8cd2300a0014137e73aee8673
Aranwind <zong002230@gmail.com>
2022-02-20 15:57:25 +0800

    无法识别+联系客服---大体完成
    剩余部分需要商议

--- app/src/main/java/com/example/demo/Suggestion.java
+++ app/src/main/java/com/example/demo/Suggestion.java
@@ -10,5 +10,6 @@ public class Suggestion extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_suggestion);
+        getSupportActionBar().hide();
     }
 }
