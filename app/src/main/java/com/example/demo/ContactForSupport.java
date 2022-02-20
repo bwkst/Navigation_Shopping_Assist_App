8826fa5c8772bac432e97b4433329ea08804a05e
bwkst <s.t.kristine.or@gmail.com>
2022-02-20 17:42:51 +0800

    小调整2

--- app/src/main/java/com/example/demo/ContactForSupport.java
+++ app/src/main/java/com/example/demo/ContactForSupport.java
@@ -9,52 +9,38 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Objects;
+
 public class ContactForSupport extends AppCompatActivity {
 
-    private TextView textView1;
-    private TextView textView2;
-
-    private String mailAdress = "test@gmail.com";//收信邮箱，虚拟
-    private String phone = "13322224444";//虚拟电话
+    private final String mailAddress = "test@gmail.com";//收信邮箱，虚拟
+    private final String phone = "13322224444";//虚拟电话
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_for_support);
-        getSupportActionBar().hide();
+        Objects.requireNonNull(getSupportActionBar()).hide();
 
         //发送邮件
-        textView1 = (TextView) findViewById(R.id.text_mail_contact);
-        textView1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //mailto:goodboy@163.com
-                Uri uri = Uri.parse("mailto:"+mailAdress);
-                Intent data = new Intent(Intent.ACTION_SENDTO);
-                data.setData(uri);
-                startActivity(Intent.createChooser(data,"请选择邮件类应用"));
-                /**另一种实现方法
-                * data.setData(Uri.parse("mailto:zong002230@gmail.com"));
-                * data.putExtra(Intent.EXTRA_CC, "");
-                * data.putExtra(Intent.EXTRA_SUBJECT, "");
-                * data.putExtra(Intent.EXTRA_TEXT, "");
-                * startActivity(data);
-                 **/
-            }
+        TextView textView1 = findViewById(R.id.text_mail_contact);
+        textView1.setOnClickListener(v -> {
+            //mailto:goodboy@163.com
+            Uri uri = Uri.parse("mailto:"+mailAddress);
+            Intent data = new Intent(Intent.ACTION_SENDTO);
+            data.setData(uri);
+            startActivity(Intent.createChooser(data,"请选择邮件类应用"));
         });
 
         //跳转到拨号页面
-        textView2 = (TextView) findViewById(R.id.text_call_contact);
-        textView2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setAction("android.intent.action.DIAL");
-                Uri uri = Uri.parse("tel:" + phone);
-                intent.setData(uri);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
-            }
+        TextView textView2 = findViewById(R.id.text_call_contact);
+        textView2.setOnClickListener(v -> {
+            Intent intent = new Intent();
+            intent.setAction("android.intent.action.DIAL");
+            Uri uri = Uri.parse("tel:" + phone);
+            intent.setData(uri);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
         });
 
         //跳转到?页面
