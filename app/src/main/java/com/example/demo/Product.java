6851ffb5fa3b2e37da77754a1d15f72e4790dc22
ING40 <2475457715@qq.com>
2022-03-08 14:18:33 +0800

    Merge branch 'main' of https://github.com/bwkst/Navigation_Shopping_Assist_App

--- app/src/main/java/com/example/demo/Product.java
+++ app/src/main/java/com/example/demo/Product.java
@@ -5,26 +5,47 @@ import android.os.Bundle;
 import android.widget.Button;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.provider.MediaStore;
-import android.view.View;
 import android.widget.ImageView;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.StrictMode;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.ImageButton;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Product extends AppCompatActivity {
+
+    private ImageView PGTPhoto;
+    private final String TAG = "tag";
+    //需要的权限数组 读/写/相机
+
+    private static final String[] PERMISSIONS_STORAGE = {Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product);
 
-        //Product -> 打开相机
-        Button PGTCamera = findViewById(R.id.b_product_taking_photo); //定位Button: b_product_take_photo
-        PGTCamera.setOnClickListener(v -> {
-            Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            startActivity(intent);
-        });
+        //跳转相机动态权限
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builder.build());
+        initView();
 
         //Product -> Go To Page: MainActivity
         Button PGTMainPage = findViewById(R.id.b_product_back); //定位Button: b_product_back
@@ -42,5 +63,86 @@ public class Product extends AppCompatActivity {
             startActivity(intent);
         });
 
+    }
+
+    private Uri ImageUri;
+    public static final int TAKE_PHOTO = 101;
+
+    private void initView() {
+        ImageButton PGTCamera = findViewById(R.id.b_product_taking_photo);
+        PGTPhoto = findViewById(R.id.b_product_taking_photo);
+
+        PGTCamera.setOnClickListener(v -> {
+            //检查是否已经获得相机的权限
+            if (verifyPermissions(Product.this, PERMISSIONS_STORAGE[2]) == 0) {
+                Log.i(TAG, "提示是否要授权");
+                ActivityCompat.requestPermissions(Product.this, PERMISSIONS_STORAGE, 3);
+            } else {
+                //已经有权限
+                toCamera();  //打开相机
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == TAKE_PHOTO) {
+            if (resultCode == RESULT_OK) {
+                try {
+                    //将拍摄的照片显示出来
+                    Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(ImageUri));
+                    PGTPhoto.setImageBitmap(bitmap);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public int verifyPermissions(Activity activity, java.lang.String permission) {
+        int Permission = ActivityCompat.checkSelfPermission(activity, permission);
+        if (Permission == PackageManager.PERMISSION_GRANTED) {
+            Log.i(TAG, "已经同意权限");
+            return 1;
+        } else {
+            Log.i(TAG, "没有同意权限");
+            return 0;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Log.i(TAG, "用户授权");
+            toCamera();
+        } else {
+            Log.i(TAG, "用户未授权");
+        }
+    }
+
+
+    //跳转相机
+    private void toCamera() {
+        //创建File对象，用于存储拍照后的图片
+        //File outputImage = new File(getExternalCacheDir(), "outputImage.jpg");
+        File outputImage = new File(getExternalCacheDir(), System.currentTimeMillis() + ".jpg");
+        if (outputImage.exists()) {
+            outputImage.delete();
+        } else {
+            try {
+                outputImage.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        //判断SDK版本高低，ImageUri方法不同
+        ImageUri = FileProvider.getUriForFile(Product.this, "com.example.demo.uploadfile4.fileprovider", outputImage);
+
+        //启动相机程序
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, ImageUri);
+        startActivityForResult(intent, TAKE_PHOTO);
     }
 }
--- app/src/main/java/com/example/demo/SettingAndHelp.java
+++ app/src/main/java/com/example/demo/SettingAndHelp.java
@@ -3,16 +3,13 @@ package com.example.demo;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SettingAndHelp extends AppCompatActivity {
 
-    private Button ContactForSupport;
-
-    private String phone = "13322224444";//虚拟电话
+    private final String phone = "13322224444";//虚拟电话
 
 
     @Override
@@ -56,29 +53,16 @@ public class SettingAndHelp extends AppCompatActivity {
             startActivity(intent);
         });
 
-/**
- *已不需要
-        //跳转到?页面
-        Button customerService = findViewById(R.id.b_customer_service); //定位Button: b_customer_service
-        customerService.setOnClickListener(v -> {
-            Intent intent=new Intent();
-            intent.setClass(SettingAndHelp.this,ContactForSupport.class); //跳转至MainActivity
-            startActivity(intent);
-        });
- **/
 
         //跳转到拨号页面
-        ContactForSupport = (Button) findViewById(R.id.b_customer_service);
-        ContactForSupport.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setAction("android.intent.action.DIAL");
-                Uri uri = Uri.parse("tel:" + phone);
-                intent.setData(uri);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
-            }
+        Button contactForSupport = findViewById(R.id.b_customer_service);
+        contactForSupport.setOnClickListener(v -> {
+            Intent intent = new Intent();
+            intent.setAction("android.intent.action.DIAL");
+            Uri uri = Uri.parse("tel:" + phone);
+            intent.setData(uri);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
         });
 
         //检查更新
